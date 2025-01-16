e1df5afc4b32d97b120b481b27cdda5296eff682
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-01-16 21:37:33 +0100

    resolver o problema na navegação e melhoria de código nos templates
    mesclar alterações entre as branches

---
+++ src/main/java/consultas/modelo/DetalhesInscricoes.java
@@ -0,0 +1,12 @@
+
+package consultas.modelo;
+
+/**
+ *
+ * @author <a href="mailto:claudiomendonca.operclaudio01@gmail.com">Cláudio Mendonça</a>
+ * 
+ * class responsável por mapear a tabela DetalhesIncricoes
+ */
+public class DetalhesInscricoes {
+    
+}
