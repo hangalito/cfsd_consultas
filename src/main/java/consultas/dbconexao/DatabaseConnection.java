238fe28a9535e1488cbcf930dd08d988ddb8180d
Bartolomeu Hangalo <bartolomeujose.manilson@gmail.com>
2025-04-27 20:20:59 +0100

    migrate database to Microsoft SQL Server

--- src/main/java/consultas/dbconexao/DatabaseConnection.java
+++ src/main/java/consultas/dbconexao/DatabaseConnection.java
@@ -1,25 +1,20 @@
 package consultas.dbconexao;
 
-import com.mysql.cj.jdbc.Driver;
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabaseConnection {
-    private static final Logger LOG = Logger.getLogger(DatabaseConnection.class.getName());
+
     private static final String USER = "root";
     private static final String PASS = "root";
-    private static final String URL = "jdbc:mysql://localhost:3306/bd_inscricoes?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String URL = "jdbc:sqlserver://OPERJOTTA\\SQLEXPRESS;databaseName=gestaocursos;encrypt=true;trustServerCertificate=true";
 
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName(Driver.class.getName());
-        } catch (ClassNotFoundException e) {
-            LOG.log(Level.SEVERE, "Class not found: {0}", e.getMessage());
-        }
+        SQLServerDriver.register();
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
